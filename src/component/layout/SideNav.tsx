683e35ab45a96b13862f930422cde6e5be0072cb
ChuloCr8v <chulocr8v@gmail.com>
2024-09-05 20:22:19 +0100

    layout fixes

--- src/component/layout/SideNav.tsx
+++ src/component/layout/SideNav.tsx
@@ -4,7 +4,7 @@ import Menu from "./Menu";
 export default function SideNav() {
   return (
     <nav className="flex flex-col w-full min-h-screen ">
-      <div className="w-full text-center mt-[24px] mb-[35px]">
+      <div className="w-full text-center mt-6 mb-8">
         <img src={Logo} alt="" className="w-16 m-auto" />
         <span className="text-base">Service Management</span>
       </div>
